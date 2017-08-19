38b1e98f1b92188b079cdc20b96b0a0284cd5a52
Endre Vegh <endre.vegh@yahoo.com>
2017-08-19 23:15:27 +0100

    reducers tests

--- frontend/src/store/action_creators/board.js
+++ frontend/src/store/action_creators/board.js
@@ -6,35 +6,39 @@ import {
 } from './../../services/minimax';
 
 export const makeHumanMove = number => (dispatch, getState) => {
-  if (
-    getState().board.settings.isCalculating ||
-    getState().board.settings.isGameWon ||
-    getState().board.board[number] !== ''
-  )
-    return;
+  return new Promise((resolve, reject) => {
+    if (
+      getState().board.settings.isCalculating ||
+      getState().board.board[number] !== ''
+    )
+      return reject('calculating');
 
-  dispatch({ type: board.INIT_CALC });
-  dispatch({ type: board.MAKE_MOVE, payload: { player: 'PLAYER', number } });
-  if (movesLeft(getState().board.board).length === 0) {
-    return dispatch({ type: board.OPEN_MODAL, payload: 'No more steps left!' });
-  }
+    dispatch({ type: board.INIT_CALC });
+    dispatch({ type: board.MAKE_MOVE, payload: { player: 'PLAYER', number } });
 
-  new Promise(resolve =>
-    setTimeout(() => resolve(getComputerMove(getState().board.board)), 0)
-  ).then(value => {
-    dispatch({
-      type: board.MAKE_MOVE,
-      payload: { player: 'COMPUTER', number: value }
-    });
-    if (isGameWon('COMPUTER', getState().board.board)) {
-      dispatch({ type: board.OPEN_MODAL, payload: 'You have lost!' });
-    } else {
-      if (movesLeft(getState().board.board).length === 0) {
+    if (movesLeft(getState().board.board).length === 0) return reject('no moves left');
+
+    setTimeout(() => resolve(getComputerMove(getState().board.board)), 0);
+  })
+    .then(value => {
+      dispatch({
+        type: board.MAKE_MOVE,
+        payload: { player: 'COMPUTER', number: value }
+      });
+      if (isGameWon('COMPUTER', getState().board.board)) {
+        dispatch({ type: board.OPEN_MODAL, payload: 'You have lost!' });
+      } else {
+        if (movesLeft(getState().board.board).length === 0) {
+          dispatch({ type: board.OPEN_MODAL, payload: 'No more steps left!' });
+        }
+        dispatch({ type: board.CLOSE_CALC });
+      }
+    })
+    .catch(error => {
+      if (error === 'no moves left') {
         dispatch({ type: board.OPEN_MODAL, payload: 'No more steps left!' });
       }
-      dispatch({ type: board.CLOSE_CALC });
-    }
-  });
+    });
 };
 
 export const newGame = () => (dispatch, getState) => {
--- frontend/test/store/action_creators/board.test.js
+++ frontend/test/store/action_creators/board.test.js
@@ -1,24 +1,114 @@
 import thunk from 'redux-thunk';
 import createMockStore from 'redux-mock-store';
+import {
+  makeHumanMove,
+  newGame
+} from './../../../src/store/action_creators/board';
+
+import {
+  movesLeft
+} from './../../../src/services/minimax';
+
+import sinon from 'sinon';
 
 const middleWares = [thunk];
-const store = createMockStore(middleWares)({
-  board: {
-    1: '',
-    2: '',
-    3: '',
-    4: '',
-    5: '',
-    6: '',
-    7: '',
-    8: '',
-    9: ''
-  },
-  settings: {
-    playerSign: 'x',
-    computerSign: 'o',
-    isCalculating: false,
-    isModalOpen: false,
-    message: ''
-  }
+
+describe('board actions creators', () => {
+  beforeAll(() => {
+    // jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+  });
+
+  afterAll(() => {
+    // jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
+  });
+  let store;
+  beforeEach(() => {
+    store = createMockStore(middleWares)({
+      board: {
+        board: {
+          1: '',
+          2: '',
+          3: '',
+          4: '',
+          5: '',
+          6: '',
+          7: '',
+          8: 'COMPUTER',
+          9: 'PLAYER'
+        },
+        settings: {
+          playerSign: 'x',
+          computerSign: 'o',
+          isCalculating: false,
+          isModalOpen: false,
+          message: ''
+        }
+      }
+    });
+  });
+
+  describe('makeHumanMove', () => {
+    it('should return a player and computer move between init and close calc if everythin is fine', async done => {
+      try {
+        await store.dispatch(makeHumanMove(1));
+        expect(store.getActions()[0].type).toBe('INIT_CALC');
+        expect(store.getActions()[1].type).toBe('MAKE_MOVE');
+        expect(store.getActions()[2].type).toBe('MAKE_MOVE');
+        expect(store.getActions()[3].type).toBe('CLOSE_CALC');
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    it('should not dispatch any action when an other one is currently running', async done => {
+      store.getState().board.settings.isCalculating = true;
+      try {
+        await store.dispatch(makeHumanMove(1));
+        expect(store.getActions().length).toBe(0);
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    it('should not dispatch any action when the board segment is already marked', async done => {
+      store.getState().board.board[1] = 'PLAYER';
+      try {
+        await store.dispatch(makeHumanMove(1));
+        expect(store.getActions().length).toBe(0);
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    it('should dispatch only a player move when board is full', async done => {
+      store.getState().board.board = {
+        1: '',
+        2: 'PLAYER',
+        3: 'PLAYER',
+        4: 'PLAYER',
+        5: 'PLAYER',
+        6: 'PLAYER',
+        7: 'PLAYER',
+        8: 'PLAYER',
+        9: 'PLAYER'
+      };
+
+      const spy = sinon.stub(movesLeft).returns([]);
+      // const myMock = jest.fn();
+// console.log(myMock());
+// > undefined
+
+// myMock.mockReturnValueOnce(10)
+      try {
+        await store.dispatch(makeHumanMove(1));
+        expect(store.getActions().length).toBe(0);
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+  });
 });
